66eaa898707f49e2cc25883c76ed341451f58def
Jasper Beachy <jbeachy@gustavus.edu>
2019-02-20 23:46:59 -0600

    Almost everything is working now

--- liri.js
+++ liri.js
@@ -11,11 +11,11 @@ var inputString = process.argv;
 
 // Parses the command line argument to capture the "command" (concert-this, spotify-this-song, movie-this, do-what-it-says) and the option
 var command = inputString[2];
-var option = inputString[3];
+
  
 switch (command) {
     case "concert-this":
-        bandIsInTown(option);
+        bandIsInTown();
         break;
     
     case "spotify-this-song":
@@ -53,21 +53,27 @@ function bandIsInTown() {
           band += inputString[i];
         }
       }
-      //if nothing selected default sets it to my brothers band coming to perform at the palace theater this weekend
-      if (band === "") band = "Sure Sure";
+      //if no band input default sets it to my brothers band coming to perform at the palace theater this weekend
+      if (band === "") {
+          band = "Sure Sure";
+      }
+      console.log("band = " + band);
       inputString+= "\n";
       appendToFile(inputString);
     axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(
     function(response) {
+        console.log("band = " + band);
         var time;
         for (var i = 0; i<response.data.length; i++) {
             time = moment(response.data[i].datetime).format('MM/DD/YYYY');
+            
             //Venue name
             console.log("________________________________________");
+            //console.log(response.data[i].lineup[0]);
             console.log(response.data[i].lineup[0] + " is playing at " + response.data[i].venue.name); 
             console.log("Latitude: " + response.data[i].venue.latitude);
             console.log("Longitude: " + response.data[i].venue.longitude);
-            console.log(response.data[i].venue.city + ", " + response.data[i].venue.region);
+            console.log(response.data[i].venue.city + "," + response.data[i].venue.region + "," + response.data[i].venue.country);
             console.log(time);
             console.log("________________________________________");
 
@@ -87,7 +93,11 @@ function OMdbSearch() {
           movie += inputString[i];
         }
       }
-      inputString+= "\n";
+    if (movie === "") {
+        movie = "The Shawshank Redemption";
+    }
+    inputString+= "\n";
+    console.log("movie = " + movie);
     appendToFile(inputString);
     axios.get("http://www.omdbapi.com/?t="+ movie + "&y=&plot=short&apikey=trilogy").then(
     function(response) {
@@ -107,7 +117,6 @@ function OMdbSearch() {
 
 function SpotifySearch() {
     var song = "";
-    
     for (var i = 3; i < inputString.length; i++) {
         if (i > 3 && i < inputString.length) {
           song = song + "+" + inputString[i];
@@ -116,11 +125,9 @@ function SpotifySearch() {
           song += inputString[i];
         }
       }
-      if (song === "") {
-          song = "The Sign Ace of Base";
-
-        }
-      spotify.search({ type: 'track', query: song }, function(err, data) {
+      if (song === "") song = "The Sign Ace of Base";
+        appendToFile(inputString);
+        spotify.search({ type: 'track', query: song }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
@@ -135,32 +142,38 @@ function SpotifySearch() {
         
       });
 }
-
-
 function doWhatItSays() {
-
     fs.readFile("random.txt", "utf8", function(error, data) {
-        
-        // If the code experiences any errors it will log the error to the console.
+        var dataArr = data.split(",");
+        inputString[2] = dataArr[0];
+        inputString[3] = dataArr[1];
+        //Holy moly that was a bug and a half
+        if (dataArr.length === 1) {
+            inputString[3] = "";
+        }
+        command = inputString[2]
         if (error) {
           return console.log(error);
         }
         else {
-            switch (inputString) {
+            switch (command) {
                 case "concert-this":
-                    bandIsInTown(option);
+                    //inputString[3] = "Sure sure";
+                    bandIsInTown();
                     break;
-                
+
                 case "spotify-this-song":
+                    //inputString[3] = "The sign ace of base";
                     SpotifySearch();
                     break;
-                
+
                 case "movie-this":
+                    //inputString[3] = "The shawshank redemption";
                     OMdbSearch()
                     break;
             }
         }
-        console.log(data);
+        console.log(dataArr);
     });
 }
     
